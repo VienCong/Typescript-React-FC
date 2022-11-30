653706c78b65b47c627336de2b4dffcf0a46657f
VienCong <wangjincong0110@163.com>
2022-11-30 21:29:24 +0800

    feat: init

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /** 一行的字符数, 如果超过会进行换行 */
+  printWidth: 140,
+  /** 一个tab代表几个空格数 */
+  tabWidth: 2,
+  /** 是否使用tab进行缩进 */
+  useTabs: false,
+  /** 字符串是否使用单引号 */
+  singleQuote: true,
+  /** 行位是否使用分号 */
+  semi: true,
+  /** 是否使用尾逗号 */
+  trailingComma: "all",
+  /** 对象大括号直接是否有空格 */
+  bracketSpacing: true,
+};
